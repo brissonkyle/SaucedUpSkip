9def5c0b19a836b61f6a4031b7f3153cc870889c
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-14 21:11:15 -0600

    Finished First Endpoint

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-import mariadb
 from flask import Flask , Response , jsonify , json ,  request
 from helpers.db_helpers import run_query
+import sys
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
 # CLIENT SIDE ENDPOINTS
 @app.get('/api/client')
 def client_get():
-    resp =  request.args
-    email = request.args.get('email')
-    firstName = request.args.get('firstName')
-    lastName = request.args.get('lastName')
-    username = request.args.get('username')
-    createdAt = request.args.get('created_at')
-    pictureUrl = request.args.get('pictureUrl')
-    run_query('SELECT ?, ?, ?, ?, ?, ? FROM client', [email,firstName,lastName,username,createdAt,pictureUrl])
+    client_data =  run_query('SELECT created_at, firstName, lastName, pictureUrl, username, email FROM client')
+    resp = []
+    for data in client_data:
+        dataObj = {}
+        dataObj['createdAt'] = data[0]
+        dataObj['firstName'] = data[1]
+        dataObj['lastName'] = data[2]
+        dataObj['pictureUrl']  = data[3]
+        dataObj['username'] = data[4]
+        dataObj['email'] = data[5]
+        resp.append(dataObj)
     return jsonify(resp), 200
 
 # @app.post('/api/client')
@@ -89,3 +94,20 @@ def client_get():
 # @app.patch('/api/menu')
 # def order_patch():
 #     return
+
+if len(sys.argv) > 1:
+    mode = sys.argv[1]
+else:
+    print('Missing required arguments')
+    exit
+
+if mode == 'testing':
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
+elif mode == 'production':
+    import bjoern
+    bjoern.run(app, '0.0.0.0', 5005)
+else :
+    print('Mode must be in testing/production')
+    exit()
