e488cd319acb427e2be65243b272a62d556ee805
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-14 23:25:54 -0600

    Fixing Second Endpoint

--- app.py
+++ app.py
@@ -43,10 +43,24 @@ def client_get():
 # def client_delete():
 #     return
 
-# # RESTAURANT SIDE ENDPOINTS
-# @app.get('/api/restaurant')
-# def restaurant_get():
-#     return
+# RESTAURANT SIDE ENDPOINTS
+@app.get('/api/restaurant')
+def restaurant_get():
+    restaurant_data =  run_query('SELECT email, name, address, city, bio, phone_num, banner_url, profileUrl FROM restaurant')
+    resp = []
+    for data in restaurant_data:
+        dataObj = {}
+        dataObj['email'] = data[0]
+        dataObj['name'] = data[1]
+        dataObj['address'] = data[2]
+        dataObj['city']  = data[3]
+        dataObj['username'] = data[4]
+        dataObj['bio'] = data[5]
+        dataObj['phone_num'] = data[6]
+        dataObj['banner_url'] = data[7]
+        dataObj['profileUrl'] = data[8]
+        resp.append(dataObj)
+    return jsonify(resp), 200
 
 # @app.post('/api/restaurant')
 # def restaurant_post():
