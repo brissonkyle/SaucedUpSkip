c8dfaed0115cecf55a407d60671f8ed87ac03d52
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-16 18:15:13 -0600

    Finished 4 Endpoints, Starting error handling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask , Response , jsonify , json ,  request
 from helpers.db_helpers import run_query
 import sys
 from flask_cors import CORS
-
+import bcrypt
 
 app = Flask(__name__)
 
@@ -19,12 +19,39 @@ def client_get():
         dataObj['pictureUrl']  = data[3]
         dataObj['username'] = data[4]
         dataObj['email'] = data[5]
+        dataObj['password'] = data[6]
         resp.append(dataObj)
     return jsonify(resp), 200
 
-# @app.post('/api/client')
-# def client_post():
-#     return
+@app.post('/api/client')
+def client_post():
+    client_resp = request.json
+    firstName = client_resp.get('firstName')
+    lastName = client_resp.get('lastName')
+    pictureUrl = client_resp.get('pictureUrl')
+    username = client_resp.get('username')
+    email = client_resp.get('email')
+    password = client_resp.get('password')
+    salt = bcrypt.gensalt()
+    client_hash = bcrypt.hashpw(password.encode(), salt)
+    if not firstName:
+        return jsonify('You must input a First Name'), 400
+    if firstName(firstName = int):
+        return jsonify('You cannot input numbers in your name'), 400
+    if not lastName:
+        return jsonify('You must input a first name'), 400
+    if lastName(lastName = int):
+        return jsonify('You cannot input numbers in your name'), 400
+    if not pictureUrl:
+        return jsonify('Add one later !!'), 200
+    if not username:
+        return jsonify('You must input a username'), 400
+    if not email:
+        return jsonify('You must input an email'), 400
+    if not password:
+        return jsonify('You must input a password'), 400
+    run_query('INSERT INTO client (firstName,lastName,pictureUrl,username,email,password) VALUES (?,?,?,?,?,?)', [firstName,lastName,pictureUrl,username,email,client_hash])
+    return jsonify('User Created'), 200
 
 # @app.patch('/api/client')
 # def client_patch():
@@ -36,8 +63,10 @@ def client_get():
 
 # # CLIENT LOGIN SIDE ENDPOINTS
 # @app.post('/api/client-login')
-# def client_post():
+# def client_login():
 #     return
+# if bcrypt.checkpw(password.encode(), client_hash.encode()):
+# return('You logged in')
 
 # @app.delete('/api/client-login')
 # def client_delete():
@@ -51,20 +80,52 @@ def restaurant_get():
     for data in restaurant_data:
         dataObj = {}
         dataObj['email'] = data[0]
-        dataObj['name'] = data[1]
-        dataObj['address'] = data[2]
-        dataObj['city']  = data[3]
-        dataObj['username'] = data[4]
-        dataObj['bio'] = data[5]
-        dataObj['phone_num'] = data[6]
-        dataObj['banner_url'] = data[7]
-        dataObj['profileUrl'] = data[8]
+        dataObj['password'] = data[1]
+        dataObj['name'] = data[2]
+        dataObj['address'] = data[3]
+        dataObj['city']  = data[4]
+        dataObj['username'] = data[5]
+        dataObj['bio'] = data[6]
+        dataObj['phone_num'] = data[7]
+        # dataObj['banner_url'] = data[7]
+        # dataObj['profileUrl'] = data[8]
         resp.append(dataObj)
     return jsonify(resp), 200
 
-# @app.post('/api/restaurant')
-# def restaurant_post():
-#     return
+@app.post('/api/restaurant')
+def restaurant_post():
+    restaurant_resp = request.json
+    email = restaurant_resp.get('email')
+    password = restaurant_resp.get('password')
+    salt = bcrypt.gensalt()
+    restaurant_hash = bcrypt.hashpw(password.encode(), salt)
+    name = restaurant_resp.get('name')
+    address = restaurant_resp.get('address')
+    city = restaurant_resp.get('city')
+    username = restaurant_resp.get('username')
+    bio = restaurant_resp.get('bio')
+    phone_num = restaurant_resp.get('phone_num')
+    if not email:
+        return jsonify('You must input an email'), 400
+    if not password:
+        return jsonify('You mus input a password')
+    if not name:
+        return jsonify('You must input a restaurant name'), 400
+    if not address:
+        return jsonify('Add one later !!'), 200
+    if not city:
+        return jsonify('You must input a city'), 400
+    if not username:
+        return jsonify('You must input a username'), 400
+    if not bio:
+        return jsonify('You must add a bio')
+    if not phone_num:
+        return jsonify('You must add a phone number')
+    if phone_num(phone_num = str):
+        return jsonify('Please input a valid phone number')
+    run_query('INSERT INTO restaurant (email,name,pictureUrl,username,email,password) VALUES (?,?,?,?,?,?)', [name,address,city,email,restaurant_hash,username,bio,phone_num])
+    return jsonify('Restaurant Created'), 200
+
 
 # @app.patch('/api/restaurant')
 # def restaurant_patch():
@@ -72,8 +133,10 @@ def restaurant_get():
 
 # # RESTAURANT LOGIN SIDE ENDPOINTS
 # @app.post('/api/restaurant-login')
-# def restaurant_post():
+# def restaurant_login():
 #     return
+# if bcrypt.checkpw(password.encode(), restaurant_hash.encode()):
+# return('You logged in')
 
 # @app.delete('/api/restaurant-login')
 # def restaurant_delete():
